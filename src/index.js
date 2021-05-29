91d028225724e0f0dee9a19272355d5c98b0fdcf
Kumavat Baluram <72209634+BALURAM1226@users.noreply.github.com>
2021-05-29 19:13:37 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
 
 
 
-const YOUR_DOMAIN = 'https://react-thgnk4.stackblitz.io/';
+const YOUR_DOMAIN = 'https://react-thgnk4.stackblitz.io/create-checkout-session';
 
 app.post('/create-checkout-session', async (req, res) => {
   const session = await stripe.checkout.sessions.create({
